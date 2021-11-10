cdf16d0104761d88bedc77f42d8447e27b2bb98e
Jaqueline Rios <jaquelinebr@gmail.com>
2021-11-10 03:28:22 -0300

    Static Charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,31 @@
 import DataTable from "assets/components/DataTable";
 import Footer from "assets/components/Footer";
 import NavBar from "assets/components/NavBar";
+import BarChart from "assets/components/BarChart";
+import DonutChart from "assets/components/DonutChart";
 
 function App() {
   return (
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">Olá mundo!</h1>
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+        
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+            <BarChart />
+          </div>
+          
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Todas Vendas</h5>
+            <DonutChart />
+          </div>
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">Todas as vendas</h2>
+        </div>
         <DataTable />
       </div>
       <Footer />
--- frontend/src/assets/components/Footer/index.tsx
+++ frontend/src/assets/components/Footer/index.tsx
@@ -4,7 +4,7 @@ function Footer() {
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://github.com/jaquelinebr" target="_blank" rel="noreferrer">Jaqueline Rios</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
-                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer" >@devsuperior.ig</a></small></p>
             </div>
         </footer>
 
